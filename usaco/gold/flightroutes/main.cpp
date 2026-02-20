1d0a454f13703d2bfd88ef68413eb8c21c81129f
Shanker Ram <91639526+reknahs@users.noreply.github.com>
2026-02-19 19:59:02 -0800

    Update main.cpp

--- usaco/gold/flightroutes/main.cpp
+++ usaco/gold/flightroutes/main.cpp
@@ -1,36 +1,29 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <cmath>
-#define ll long long
-#define pii pair<int, int>
 using namespace std;
 
-int parities[751][751];
+bool parities[751][751];
+bool edges[751][751];
 
 int main() {
     int n;
     cin >> n;
-    for(int i = 0; i < n; i++) {
-        for(int j = i+1; j < n; j++) {
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
             char c;
             cin >> c;
-            parities[i][j] = c-'0';
+            parities[i][j] = c - '0';
         }
     }
-
-    int count = 0;
+    int ans = 0;
     for(int i = 0; i < n; i++) {
-        vector<int> connections(0);
         for(int j = i+1; j < n; j++) {
-            int combs = 0;
-            for(int c: connections) combs += parities[c][j];
-            if(combs%2 != parities[i][j]) {
-                count++;
-                connections.push_back(j);
+            int total = parities[i][j];
+            for(int k = i+1; k < j; k++) {
+                total += edges[i][k]*parities[k][j];
             }
+            edges[i][j] = total%2;
+            ans += edges[i][j];
         }
     }
-    cout << count;
+    cout << ans;
 }
